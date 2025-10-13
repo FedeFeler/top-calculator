feb6315fc81b4bbe686723c7db64611b69aa7d88
Federico <fledesmarubulotta@gmail.com>
2025-10-13 10:37:11 -0300

    fix identation

--- js/div-generator.js
+++ js/div-generator.js
@@ -1,32 +1,31 @@
-const buttonContainer = document.getElementById('button-container')
+const buttonContainer = document.getElementById("button-container");
 
-dotBtn = document.createElement('button');
-dotBtn.classList.add('dot-btn');
-dotBtn.textContent = '.';
+dotBtn = document.createElement("button");
+dotBtn.classList.add("dot-btn");
+dotBtn.textContent = ".";
 buttonContainer.appendChild(dotBtn);
 
 for (let i = 9; i >= 0; i--) {
-  numberBtn = document.createElement('button');
-  numberBtn.classList.add('number-btn');
+  numberBtn = document.createElement("button");
+  numberBtn.classList.add("number-btn");
   numberBtn.textContent = i;
   buttonContainer.appendChild(numberBtn);
 }
 
-const upperBtnCont = document.getElementById('upper-btn');
+const upperBtnCont = document.getElementById("upper-btn");
 const symbolsUpper = ["CE", "/", "X", "-"];
 symbolsUpper.forEach((item) => {
-  let upperBtn = document.createElement('button');
-  upperBtn.classList.add('upper-btn');
+  let upperBtn = document.createElement("button");
+  upperBtn.classList.add("upper-btn");
   upperBtn.textContent = item;
   upperBtnCont.appendChild(upperBtn);
-})
+});
 
 const rightBtnCont = document.getElementById("right-btn");
 const symbolsRight = ["+", "Enter"];
 symbolsRight.forEach((item) => {
-  let rightBtn = document.createElement('button');
-  rightBtn.classList.add('right-btn');
+  let rightBtn = document.createElement("button");
+  rightBtn.classList.add("right-btn");
   rightBtn.textContent = item;
   rightBtnCont.appendChild(rightBtn);
-})
-
+});
